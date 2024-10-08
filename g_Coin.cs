173435108690326904001cf69204cce85ae8d9fe
gary909 <25279957+gary909@users.noreply.github.com>
2024-10-08 11:31:14 +0200

    coin and block code working fully

---
+++ g_Coin.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using MoreMountains.Tools;
+using MoreMountains.CorgiEngine;
+
+// This is used instead of the MM coin.cs script.  USe in conjunction 
+// with g_BonusBlock so the coin launches out of the block and coins collected
+// animated properly etc
+
+namespace MoreMountains.CorgiEngine
+{
+    [AddComponentMenu("Corgi Engine/Items/g_Coin")]
+    public class g_Coin : MonoBehaviour
+    {
+        [Header("Coin")]
+        public int PointsToAdd = 10;
+
+        // This method is called when the block's movement is complete
+        public void Disappear()
+        {
+            // Trigger the points event
+            CorgiEnginePointsEvent.Trigger(PointsMethods.Add, PointsToAdd);
+
+            // Destroy the coin game object
+            Destroy(gameObject);
+        }
+    }
+}
