94d235bf012b114a16e006ac02edea6abe259317
gary909 <25279957+gary909@users.noreply.github.com>
2024-11-27 14:29:44 +0100

    improved performance

---
+++ FireBarRotate3.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Rotates the fireball/firebar sprites
+// Added clockwise toggle
+// Added coroutine for betterer performance
+
+public class g_FireBarRotate : MonoBehaviour
+{
+    public float rotationSpeed = 50f;
+    public bool rotateClockwise = true;
+    public float updateInterval = 0.02f; // Interval between rotations
+
+    void Start()
+    {
+        StartCoroutine(RotateFireBar());
+    }
+
+    IEnumerator RotateFireBar()
+    {
+        while (true)
+        {
+            float direction = rotateClockwise ? -1f : 1f;
+            transform.Rotate(0, 0, direction * rotationSpeed * updateInterval);
+            yield return new WaitForSeconds(updateInterval);
+        }
+    }
+}
